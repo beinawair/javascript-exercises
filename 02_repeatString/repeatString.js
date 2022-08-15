512b0075133c4f587df2bf530783e5d3e1471ca1
beinawair <notifier.id@gmail.com>
2022-08-15 17:41:21 +0800

    practice to exercise 07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let stringHolder = "";
+    
+    for (let i = 0; i < number; i++) {
+        stringHolder += string;
+    }
+    
+    if(number < 0) return "ERROR";
 
+    return stringHolder;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...value) {
+    const arr = value[0];
+    const newArray = [];
 
+    arr.forEach(val => {
+        if(!value.includes(val)) {
+            newArray.push(val)
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
