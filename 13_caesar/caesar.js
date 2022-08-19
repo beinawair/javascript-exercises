92e747c73e68a56e1935dcced0a12ac62ecabcaf
beinawair <notifier.id@gmail.com>
2022-08-19 22:12:27 +0800

    completing all exercises

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,20 @@
-const caesar = function() {
-
+const caesar = function(word, num) {
+    return word.split("").map(char => shiftWord(char, num)).join("");
 };
 
+const codeSet = code => (code < 97 ? 65 :   97);
+
+const mod = (a, b) => (a % b + b) % b;
+
+const shiftWord = (char, num) => {
+    const code = char.charCodeAt();
+
+    if((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        return String.fromCharCode(
+            mod(code + num - codeSet(code), 26) + codeSet(code)
+        )
+    }
+    return char;
+}
 // Do not edit below this line
 module.exports = caesar;
